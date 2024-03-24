94f94b787aa3d9923b96bd6963a2284bee253670
Paavo Becker <paavo.becker@xcor.org>
2024-03-24 09:07:44 +0100

    Add tests for FileManager

--- Tests/PersistenceTests/Extensions/FileManagerTests.swift
+++ Tests/PersistenceTests/Extensions/FileManagerTests.swift
@@ -6,9 +6,18 @@
 //
 
 import XCTest
+import Foundation
+import Persistence
 
 final class FileManagerTests: XCTestCase {
-    func testDirectoryExists_returnTrue_whenDirectoryExistsAtPath() {
+    func testDirectoryExists_returnTrue_whenDirectoryExistsAtPath() throws {
+        // GIVEN
+        let tempDirectoryUrl = FileManager.default.temporaryDirectory
         
+        // WHEN
+        let output = FileManager.default.directoryExists(atPath: tempDirectoryUrl.path())
+        
+        // THEN
+        XCTAssertTrue(output)
     }
 }
