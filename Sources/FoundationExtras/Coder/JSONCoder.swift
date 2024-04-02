eb08dfdf6723ee4063f73d692ba08b23d20b1672
Paavo Becker <paavo.becker@xcor.org>
2024-04-02 21:14:57 +0200

    Add tests for JSONCoder

--- Sources/FoundationExtras/Coder/JSONCoder.swift
+++ Sources/FoundationExtras/Coder/JSONCoder.swift
@@ -6,15 +6,22 @@
 
 import Combine
 import Foundation
+import XCTestDynamicOverlay
 
 // MARK: - JSONCoder
 
+/// A coder that can encode into and decode from JSON `Data`.
 public class JSONCoder: Coder {
     public typealias Output = Data
 
     private let encoder: JSONEncoder
     private let decoder: JSONDecoder
-
+    
+    /// Creates a JSONCoder.
+    ///
+    /// - Parameters:
+    ///   - encoder: The JSONEncoder that will be used for encoding operations.
+    ///   - decoder: The JSONDecoder that will be used for decoding operations.
     public init(
         encoder: JSONEncoder = JSONEncoder(),
         decoder: JSONDecoder = JSONDecoder()
@@ -32,8 +39,9 @@ public class JSONCoder: Coder {
     }
 }
 
-extension JSONCoder {
-    public var json: AnyCoder<Data> {
+extension AnyCoder {
+    public static var json: AnyCoder<Data> {
         JSONCoder().eraseToAnyCoder()
     }
 }
+
