bd96cb65f068a648c28a8ea232f6a29b380fddec
Paavo Becker <paavo.becker@xcor.org>
2024-06-17 22:12:05 +0200

    Change JSONCoder into a struct

--- Sources/FoundationExtras/Coder/JSONCoder.swift
+++ Sources/FoundationExtras/Coder/JSONCoder.swift
@@ -11,7 +11,7 @@ import XCTestDynamicOverlay
 // MARK: - JSONCoder
 
 /// A coder that can encode into and decode from JSON `Data`.
-public class JSONCoder: Coder {
+public struct JSONCoder: Coder {
     public typealias Output = Data
 
     private let encoder: JSONEncoder
